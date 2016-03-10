0380d5b1efbb9333c95c7ad69b0bbca2f8e1a501
padrinoo2 <padrinoo200@gmail.com>
2016-03-10 22:07:09 +0330

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -224,10 +224,8 @@ function create_config( )
     "all",
     "leave_ban",
 	"supergroup",
-	"whitelist",
-	"msg_checks"
-    },
-    sudo_users = {110626080,103649648,111020322,0,tonumber(our_id)},--Sudo users
+	"whitelist",                                                     
+{156218623},--Sudo users
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v4
 An advanced administration bot based on TG-CLI written in Lua
